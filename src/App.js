27567928015436ea6017e3212d0e962f6d8851b2
Christian Cervenka <christian.cervenka@oevp.at>
2021-10-14 14:59:54 +0200

    commitment button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+// import React from 'react'
+import Header from "./components/Header";
 
 function App() {
 
 
   return (
     <div className="container">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-      <h1>Hello World from REACT</h1>
+      <Header />
     </div>
 
     
   );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,27 @@
-import React from 'react'
+// import React from 'react'
+import PropTypes from 'prop-types'
 
-const header = () => {
+const Header = ({title}) => {
     return (
-        <header>
-            
+        <header className='header'>
+            <h1>{title}</h1>
+            <button className='btn'>Add</button>
         </header>
     )
 }
 
-export default header
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// -> Css in JS | SYLE für <h1 style={headingStyle}>{title}</h1>
+// const headingStyle = {
+//     color: '#010101',
+//     backgroundColor: '#f2f2f2'
+// }
+
+export default Header
